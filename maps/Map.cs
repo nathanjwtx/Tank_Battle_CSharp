8cecc1a0d0bc585e01c3be484e0929d62e1e5439
Nathan <dynamonath@nathanjwtx.me>
2018-07-15 08:21:22 -0500

    Updated camera and map

---
+++ maps/Map.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Map : Node2D
+{
+    public TileMap Ground;
+    public Camera2D Camera2D;
+
+    public override void _Ready()
+    {
+        SetCameraLimits();
+
+    }
+
+    public void SetCameraLimits()
+    {
+        var grnd = (TileMap) GetNode("Ground");
+        var camera = (Camera2D) GetNode("Player/Camera2D");
+        var mapLimits = grnd.GetUsedRect();
+        var mapCellsize = grnd.CellSize;
+        camera.LimitLeft = Convert.ToInt32(mapLimits.Position.x * mapCellsize.x);
+        camera.LimitRight = Convert.ToInt32(mapLimits.End.x * mapCellsize.x);
+        camera.LimitTop = Convert.ToInt32(mapLimits.Position.y * mapCellsize.y);
+        camera.LimitBottom = Convert.ToInt32(mapLimits.End.y * mapCellsize.y);
+    } 
+}
